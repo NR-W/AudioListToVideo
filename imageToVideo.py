42e5369b3a98ca02e902d809136addaa0a07c482
naren <naren@watthora.com>
2021-10-01 04:38:46 -0400

    Length of video file should be a function of the length of the audio file.

--- imageToVideo.py
+++ imageToVideo.py
@@ -4,14 +4,20 @@ from moviepy.editor import *
 # Sample image code taken from: https://www.tutorialexample.com/python-moviepy-convert-images-png-jpg-to-video-python-moviepy-tutorial/
 # Sample audio code taken from: https://stackoverflow.com/a/55920417
 
-# Images credit: https://www.pexels.com/@maumascaro
-# files = ['img/712786.jpg', 'img/9192283.jpg']
-# clip = ImageSequenceClip(files, fps = 4, durations=[100,1000])
-clip = ImageClip("img/712786.jpg", duration=100)
-
 # Sound credits: https://soundbible.com/royalty-free-sounds-1.html
 audioclip = AudioFileClip("audio/bells-tibetan-daniel_simon.mp3")
 new_audioclip = CompositeAudioClip([audioclip])
-clip.audio = new_audioclip
 
+# Give the video 2 more seconds of runtime so that the end is not too abrupt
+audioduration = 0
+audioduration = audioclip.duration
+videoduration = audioduration + 2
+
+# Images credit: https://www.pexels.com/@maumascaro
+# files = ['img/712786.jpg', 'img/9192283.jpg']
+# clip = ImageSequenceClip(files, fps = 4, durations=[100,1000])
+# clip = ImageClip("img/712786.jpg", duration=100)
+clip = ImageClip("img/9192283.jpg", duration=videoduration)
+
+clip.audio = new_audioclip
 clip.write_videofile("video.mp4", fps = 24)
